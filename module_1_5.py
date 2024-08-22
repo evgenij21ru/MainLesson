55b3d07a19cf62afc4166281a692a583039e24c9
evgenij21ru <evgenij21ru@gmail.com>
2024-08-22 20:53:21 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,5 +1,9 @@
 immutable_var = 1, 2, 3, 'evgenij', True
 print('immutable tuple:', immutable_var)
+#immutable_var[1] = 12
+#    ~~~~~~~~~~~~~^^^
+#TypeError: 'tuple' object does not support item assignment
+#кортеж не поддерживает обращение по элементам
 mutable_list = [1, 2, 3, 4, 'stop', True]
 print('Mutable list:', mutable_list)
 mutable_list[3] = 100
@@ -9,4 +13,4 @@ mutable_list.extend('gays')
 print('Mutable list modific_2:', mutable_list)
 mutable_list.remove(False)
 print('Mutable list modific_3:', mutable_list)
-# print(2 in mutable_list )
+#print(5 in mutable_list )
