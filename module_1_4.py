2a048d5e2d8aaebc266159dfbfaaaa352599ad75
evgenij21ru <evgenij21ru@gmail.com>
2024-08-22 12:02:53 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_strong = input('Введите любой текст:')
+print('В вашем тексте',len(my_strong),'символов')
+print('Текст в верхнем регистре:',my_strong.upper())
+print('Текст в нижнем регистре:',my_strong.lower())
+print('Текст без пробелов:',my_strong.replace(' ',''))
+print('Первый символ строки',str(my_strong[0]))
+print('Последний символ строки',str(my_strong[-1]))
+
